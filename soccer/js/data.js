16d2848e7fe1da11dcc14c266902362afd410044
Andrii <aatkachenko23@gmail.com>
2019-08-12 23:38:36 +0300

    fixed game parsing

--- soccer/js/data.js
+++ soccer/js/data.js
@@ -74,6 +74,10 @@ function getGameList(dateKey, callback) {
     }
 }
 
+function isDigit(c) {
+    typeof c === 'string' && c.length == 1 && c >= '0' && c <= '9';
+}
+
 function makeGameList(arr) {
 /*
 game: id, time, league, host/guest : name, rank
@@ -87,7 +91,9 @@ game: id, time, league, host/guest : name, rank
 
     arr.forEach(function(data){
         if (data[key] && data[key + '-id']) {
-            var str = data[key];
+            var str = data[key].trim();
+            str = str.replace(/\d+ - \d+$/, ""); //remove first half result at the end 
+            str = str.replace(/\d+ - \d+/, " - "); //remove part of the current result in the middle
             var id = data[key + '-id'];
             var game = {};
             game['id'] = id.replace('tr1_', '');
@@ -98,6 +104,7 @@ game: id, time, league, host/guest : name, rank
                 return;
             }
             idx += 3;
+            // maybe current time
             started = str[idx] != ' ';
             running = !isNaN(str[idx]) || str.substr(idx, 2) == 'HT';
             if (started && !running) {
@@ -106,15 +113,18 @@ game: id, time, league, host/guest : name, rank
 //ENG U23 D113:3081[18] Huddersfield U232 - 1Hull City U23 [10]1 - 1                    
 //  TUR U1913:0090+ Bucaspor U192 - 2Altay Spor KulubuU19 1 - 2
             if (started) {
-                idx += 2;
-                if (str[idx + 3] == "+") {
+                idx += 1;
+                if (isDigit(str[idx])) {
                     idx += 1;
-                }
+                    if (str[idx] == "+") {
+                        idx += 1;
+                    }
+               }
             }
-            var idx2 = str.indexOf("]", idx);
-            var idx3 = str.indexOf("-", idx2 == -1 ? idx : idx2);
-            var host =  str.substr(idx, idx3 - idx);
-            var guest =  str.substr(idx3 + 1);
+            str = str.substr(idx);
+            var teamSeparatorIdx = str.search(/-(?=[^\]\[-]+\[.+$|[^\]\[-]+$)/);
+            var host =  str.substr(0, teamSeparatorIdx).trim();
+            var guest =  str.substr(teamSeparatorIdx + 1).trim();
             game['host'] = parseTeam(host);
             game['guest'] = parseTeam(guest);
             if (game['host'] && game['guest']) {
