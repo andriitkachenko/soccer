4039052ccb967ffb6f8bdd96e72449a1daf3468d
Andrey Tkachenko <aatkachenko23@gmail.com>
2020-02-02 22:31:45 +0200

    updated sql-s

--- soccer/sql/soccer.sql
+++ soccer/sql/soccer.sql
@@ -7,16 +7,16 @@
 -- truncate table `games`;
 
 create table if not exists `games` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `game_id` int(11) NOT NULL,
-    `league` varchar(255) DEFAULT NULL,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `game_id` INT UNSIGNED NOT NULL,
+    `league` VARCHAR(30) DEFAULT NULL,
     `start_at` TIMESTAMP NOT NULL,
-    `host` varchar(255) NOT NULL,
-    `host_rank` varchar(30) DEFAULT NULL,
-    `guest` varchar(255) NOT NULL,
-    `guest_rank` varchar(30) DEFAULT NULL,
-    `finished` int(1) DEFAULT 0,
-    `description` varchar(255) DEFAULT NULL,
+    `host` VARCHAR(255) NOT NULL,
+    `host_rank` VARCHAR(30) DEFAULT NULL,
+    `guest` VARCHAR(255) NOT NULL,
+    `guest_rank` VARCHAR(30) DEFAULT NULL,
+    `finished` TINYINT(1) DEFAULT 0 NOT NULL,
+    `description` VARCHAR(255) DEFAULT NULL,
     `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`game_id`),
@@ -24,29 +24,29 @@ create table if not exists `games` (
 ) ENGINE=InnoDB, CHARACTER SET=UTF8;
 
 create table if not exists `game_corrections` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `game_id` int(11) NOT NULL,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `game_id` INT UNSIGNED NOT NULL,
     `current_time` TIMESTAMP NOT NULL,
     `game_time` varchar(8) NOT NULL,
-    `first_half` int(1) NOT NULL,
+    `first_half` tinyint(1) NOT NULL,
     `score` varchar(30) NOT NULL,
     PRIMARY KEY (`id`),
     KEY (`game_id`),
     UNIQUE KEY(`game_id`, `first_half`),
-    CONSTRAINT FK_game_times_game_id FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`) 
+    CONSTRAINT FK_game_corrections_game_id FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`) 
         ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB, CHARACTER SET=UTF8;
 
 create table if not exists `games_version` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB, CHARACTER SET=UTF8;
 -- INSERT INTO  `games_version` VALUES ();
 
 create table if not exists `game_events` (
-    `id` int(11) not null AUTO_INCREMENT,
-    `game_id` int(11) not null,
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `game_id` INT UNSIGNED NOT NULL,
     `timestamp` TIMESTAMP NOT NULL,
     `host` tinyint(1) not null,
     `event` enum('gl', 'sg', 'sh', 'rc', 'yc', 'fl', 'bp', 'ck', 'of', 'hd', 'sv', 'st', 'ic', 'as') NOT NULL 
@@ -66,7 +66,7 @@ create table if not exists `game_events` (
             ic = interceptions,
             as = assists
         ",    
-    `amount` int not null default 1,
+    `amount` TINYINT NOT NULL DEFAULT 1,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`game_id`, `host`, `event`, `amount`),
     CONSTRAINT FK_game_events_game_id FOREIGN KEY (`game_id`) REFERENCES `games`(`game_id`) 
