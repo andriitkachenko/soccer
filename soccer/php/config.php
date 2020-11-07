dfe42aa4cca6d376029241403a2b48ad1423a540
Andrey Tkachenko <aatkachenko23@gmail.com>
2020-11-07 10:50:35 +0200

    log config refactoring

--- soccer/php/config.php
+++ soccer/php/config.php
@@ -1,12 +1,14 @@
 <?php
 
-const DATA_FILE = __DIR__ . '/../logs/last_game_list.json';
-const LAST_PARSEHUB_RESPONSE_FILE = __DIR__ . "/../logs/last_parsehub_response.txt";
+const LOG_DIR = __DIR__ . '/../logs';
 
-const ACCESS_LOG = __DIR__ . "/../logs/access.log";
-const PARSEHUB_LOG = __DIR__ . "/../logs/parsehub.log";
-const CRON_LOG = __DIR__ . "/../logs/cron.log";
-const ERROR_LOG = __DIR__ . "/../logs/error.log";
+const DATA_FILE = LOG_DIR . '/last_game_list.json';
+const LAST_PARSEHUB_RESPONSE_FILE = LOG_DIR . "/last_parsehub_response.txt";
+
+const ACCESS_LOG = LOG_DIR . "/access.log";
+const PARSEHUB_LOG = LOG_DIR . "/parsehub.log";
+const CRON_LOG = LOG_DIR . "/cron.log";
+const ERROR_LOG = LOG_DIR . "/error.log";
 
 define('MAX_LOG_SIZE', 2 * pow(1024, 2)); // 2Mb
 
