04324fdaec02b354ddf8f75b8fbc75a66cb4df22
Andrey Tkachenko <aatkachenko23@gmail.com>
2020-11-11 21:27:00 +0200

    frontend - show game time

--- soccer/frontend/src/components/app_games/app_games.js
+++ soccer/frontend/src/components/app_games/app_games.js
@@ -18,16 +18,64 @@ class AppGames extends React.Component {
             return;
 
         this.updating = true;
-        const stats = dataService
+        dataService
             .getLastStats()
             .then((stats) => {
                 if (stats !== false) {
-                   this.setState({games : Object.values(stats).sort((a, b) => { return b.min - a.min;})})
+                    const games = Object.values(stats)
+                        .map((g) => {
+                            const startUTC = this.getLocalSeconds(g.start_real);
+                            const nowUTC = this.getLocalSeconds();
+                            var m = Math.floor(((nowUTC - startUTC) / 60));
+                            if (g.state === 3) m = 45 + m
+                            else if (g.state === 4) m = 90 + m
+                            g.time = m;
+                            return g;
+                        })
+                        .sort((a, b) => { 
+                            var s = b.state - a.state; 
+                            if (s) return s;
+                            s = b.time - a.time;
+                            if (s) return s;
+                            s = a.league === b.league ? 0 : (a.league > b.league ? 1 : -1);
+                            if (s) return s;
+                            s = a.id - b.id;
+                            return s;
+                        })
+                        .map((g) => {
+                            var m = '' + g.time;
+                            if (g.state === 1 && g.time > 45) m = '45+' + (g.time - 45)
+                            if (g.state === 2) m = 'HT'
+                            if (g.state === 3 && g.time > 90) m = '90+' + (g.time - 90)
+                            g.time = m;
+                            return g;
+                        });
+                    this.setState({games : games});
                 }
             });
         this.updating = false;
     }
 
+    getLocalSeconds(utcString) {
+        const local = utcString ? this.getDateLocal(utcString) : new Date();
+        return Math.floor(local.getTime() / 1000);
+    }
+
+    getDateLocal(utcString) {
+        var local = new Date(this.makeISOUTCString(utcString)).toString();
+        return new Date(local);
+    }
+
+    getLocalTimeString(utcString) {
+        var local = this.getDateLocal(utcString);
+        const h = local.getHours();        
+        const m = local.getMinutes();        
+        return (h < 10 ? '0' : '') + h + ":" + (m < 10 ? '0' : '') + m;
+    }
+
+    makeISOUTCString(utcString) {
+        return utcString.replace(' ', 'T') + 'Z';
+    }
     componentDidMount() {
         this.updateState();
         if (!this.timerId) 
@@ -45,17 +93,13 @@ class AppGames extends React.Component {
             return <span>No live games</span>
         }
         const gameList = games.map((game) => {
-            const time = new Date(game.start_time);
-            const h = time.getHours();
-            const m = time.getMinutes();
-            
             return (
                 <li className = 'item-list list-group-item' key={game.id}>
                     <table className="table">
                         <tbody>
                         <tr>
                             <td className='league'>{game.league} {}</td>
-                            <td className='time game' rowSpan="2">{game.min}</td>
+                            <td className='time game' rowSpan="2">{game.time}</td>
                             <td className='teams'>{ game.host } <span className="rank"> { game.host_rank ? game.host_rank : ''}</span></td>
                             <td className='stat gl'>{game.host_stat.gl}</td>
                             <td className='time' rowSpan="2">{game.min}</td>
@@ -65,7 +109,7 @@ class AppGames extends React.Component {
                             <td className='stat'>{game.host_stat.rc}</td>
                         </tr>
                         <tr>
-                            <td className='league'>{(h < 10 ? '0' : '') + h + ":" + (m < 10 ? '0' : '') + m}</td>
+                            <td className='league'>{this.getLocalTimeString(game.start_time)}</td>
                             <td className='teams'>{game.guest}</td>
                             <td className='stat gl'>{game.guest_stat.gl}</td>
                             <td className='stat long'>{game.guest_stat.sh} - {game.guest_stat.sg}</td>
