e7ab33d3e66019242917af40babd41702bc23420
Andrey Tkachenko <aatkachenko23@gmail.com>
2021-06-28 09:48:14 +0300

    shorter run data in logs

--- soccer/services/parsehub/parsehub_utils.php
+++ soccer/services/parsehub/parsehub_utils.php
@@ -1,7 +1,7 @@
 <?php
 
 function reduceRunData($run) {
-    $params = ['status', 'run_token', 'data_ready', 'is_empty', 'start_time', 'start_running_time'];
+    $params = ['status', 'run_token', 'data_ready', 'is_empty'];//, 'start_time', 'start_running_time'];
     $log = [];
     foreach($params as $p) {
         if (isset($run[$p])) {
